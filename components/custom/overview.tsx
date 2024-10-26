c2a844a6c7f3126fb66b60e877fb78ea0d39840a
David Rhodes <david@espressocode.tech>
2024-10-26 18:06:59 -0500

    Refactor icon components and update import statements in overview.tsx

--- components/custom/overview.tsx
+++ components/custom/overview.tsx
@@ -19,7 +19,7 @@ export const Overview = () => {
           <span>+</span>
           <MessageIcon />
           <span>+</span>
-          <VercelIcon />
+          <LogoOpenAI />
         </p>
         <p>
           This is an open source Chatbot template built with Next.js and the AI
@@ -28,6 +28,23 @@ export const Overview = () => {
           function on the server and the{' '}
           <code className="rounded-md bg-muted px-1 py-0.5">useChat</code> hook
           on the client to create a seamless chat experience.
+        </p>
+        <p>
+          Abstraction Layer: The SDK provides an abstraction layer over
+          different LLMs, allowing developers to interact with a unified API.
+          This means zero worries about the specific details of each LLM API.
+        </p>
+        <p>
+          Unified API: By offering a consistent API, the SDK makes it easier to
+          switch between different LLMs or use multiple LLMs within the same
+          application. This is particularly useful for experimenting with
+          different models to see which one best suits your needs.
+        </p>
+        <p>
+          UI Components: The SDK includes pre-built UI components that can be
+          easily integrated into your React applications. These components
+          handle common tasks such as text input, displaying responses, and
+          streaming text.
         </p>
         <p>
           You can learn more about the AI SDK by visiting the{' '}
@@ -40,16 +57,15 @@ export const Overview = () => {
           </Link>
           .
         </p>
-        <p>
-          This project is available for public use from my{' '}
+        <p className="flex flex-row items-center gap-2">
+          This project is available for public use from the{' '}
           <Link
-            className="text-blue-500 dark:text-blue-400"
+            className="text-blue-500 dark:text-blue-400 flex items-center gap-1"
             href="https://github.com/dfrho/nextjs-ai-chatbot"
             target="_blank"
           >
-            GitHub <GitHubIcon /> repo
+            <GitHubIcon /> GitHub repo.
           </Link>
-          .
         </p>
       </div>
     </motion.div>
