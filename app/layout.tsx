c8e6e420039ea39268ae2eb8c2a06847f362636c
David Rhodes <david@espressocode.tech>
2024-10-28 11:27:52 -0500

    Refactor environment variables and add PostHog provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Toaster } from 'sonner';
 
 import { Navbar } from '@/components/custom/navbar';
 import { ThemeProvider } from '@/components/custom/theme-provider';
+import Providers from './providers';
 
 import './globals.css';
 
@@ -79,9 +80,11 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Toaster position="top-center" />
-          <Navbar />
-          {children}
+          <Providers>
+            <Toaster position="top-center" />
+            <Navbar />
+            {children}
+          </Providers>
         </ThemeProvider>
       </body>
     </html>
---
+++ app/providers.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+import { CookieBanner } from '@/components/custom/cookie-banner';
+
+if (typeof window !== 'undefined') {
+  const consent = localStorage.getItem('cookie-consent');
+
+  posthog.init(process.env.POSTHOG_KEY || '', {
+    api_host: process.env.POSTHOG_HOST || 'https://us.i.posthog.com',
+    person_profiles: 'always', // or 'identified_only' to create profiles for only signed in users
+    loaded: (posthog) => {
+      //   if (process.env.NODE_ENV === 'development') posthog.debug();
+      if (!consent || consent === 'false') {
+        posthog.opt_out_capturing();
+      }
+    },
+    capture_pageview: false, // We'll capture these manually
+  });
+}
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PostHogProvider client={posthog}>
+      {children}
+      <CookieBanner />
+    </PostHogProvider>
+  );
+}
