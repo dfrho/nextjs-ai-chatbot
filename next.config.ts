b0ff2a073bd977917c6430b544b6b5d5f659d4a8
David Rhodes <david@espressocode.tech>
2024-10-28 12:35:29 -0500

    debug state

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+const path = require('path');
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -8,6 +9,10 @@ const nextConfig: NextConfig = {
   env: {
     POSTHOG_KEY: process.env.POSTHOG_KEY,
     POSTHOG_HOST: process.env.POSTHOG_HOST,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
 };
 
